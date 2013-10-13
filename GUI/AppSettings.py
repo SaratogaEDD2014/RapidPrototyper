2444054eff3da06347fbfa45ae29f973731062be
SkottyK <skrulcik@gmail.com>
2013-10-13 14:24:49 -0400

    Update PATH variables to use with each test machine

--- GUI/AppSettings.py
+++ GUI/AppSettings.py
@@ -3,7 +3,9 @@ import wx
 #UI
 defaultColor=wx.Colour(0,0,0)
 
-PATH="/home/ubuntu/Documents/RapidPrototyper/GUI"
+#PATH="/home/ubuntu/Documents/RapidPrototyper/GUI/"  #Ubuntu
+PATH="/Users/Scott/Documents/Design/Ultimaker/GUI/" #MAC
+#PATH="C:/Users/Scott/Documents/RapidPrototyper" #PC (Microsoft)
 IMAGE_PATH=PATH+"images/"
 
 #Print
