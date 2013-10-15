ee0c501651b4a963b6ca86746c3ee8ae26dddd01
SkottyK <skrulcik@gmail.com>
2013-10-15 14:27:25 -0400

    Update App.py

--- GUI/App.py
+++ GUI/App.py
@@ -1,35 +1,34 @@
 import wx
 import copy
 import AppSettings
-import UMToolbar
-import DataEntryRegion
-import ShapesRegion
+import BubbleMenu
 
 class ProtoFrame(wx.Frame):
 
 
     def __init__(self, parent, id, title):
         wx.Frame.__init__(self, parent, id, title, wx.DefaultPosition, wx.Size(800, 510))
-        self.imagePath=AppSettings.IMAGE_PATH
+        self.imagePath=AppSettings.IMAGE_PATH+"Main/"
         self.title=title
-
-        toolbar=UMToolbar.UMToolbar(self, -1)
-        shapes=ShapesRegion.ShapesRegion(self)
-        dataEntry=DataEntryRegion.DataEntryRegion(self)
-
+        self.menu=BubbleMenu.BubbleMenu(self, wx.Bitmap(self.imagePath+"BubbleTitle.png"), "Shape Menu")
+        self.buttonList=[BubbleMenu.BubbleButton(   self.menu, wx.Bitmap(self.imagePath+"buttonTemplate1.png"), wx.Bitmap(self.imagePath+"buttonPressed1.png")),
+                            BubbleMenu.BubbleButton(self.menu, wx.Bitmap(self.imagePath+"buttonTemplate2.png"), wx.Bitmap(self.imagePath+"buttonPressed2.png")),
+                            BubbleMenu.BubbleButton(self.menu, wx.Bitmap(self.imagePath+"buttonTemplate3.png"), wx.Bitmap(self.imagePath+"buttonPressed3.png")),
+                            BubbleMenu.BubbleButton(self.menu, wx.Bitmap(self.imagePath+"buttonTemplate4.png"), wx.Bitmap(self.imagePath+"buttonPressed4.png")),
+                            BubbleMenu.BubbleButton(self.menu, wx.Bitmap(self.imagePath+"buttonTemplate5.png"), wx.Bitmap(self.imagePath+"buttonPressed5.png")),
+                            BubbleMenu.BubbleButton(self.menu, wx.Bitmap(self.imagePath+"buttonTemplate6.png"), wx.Bitmap(self.imagePath+"buttonPressed6.png")),]
+        self.menu.AddMany(self.buttonList)
         sizer=wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(toolbar)                 #,        (0,0),  (4,80))
-        sizer.Add(shapes)             #,      (4, 41),(39,39))
-        sizer.Add(dataEntry)       #,(44,41),(4,39))
+        sizer.Add(self.menu)
         self.SetSizer(sizer)
 
 
+def main():
+    ProtoApp = wx.App()
+    frame = ProtoFrame(None, -1, 'Blue Streaks EDD')
+    frame.Show(True)
+    ProtoApp.MainLoop()
 
-class MyApp(wx.App):
-    def OnInit(self):
-        frame = ProtoFrame(None, -1, 'Blue Streaks EDD')
-        frame.Show(True)
-        return True
+if __name__ == '__main__':
+    main()
 
-app = MyApp(0)
-app.MainLoop()
