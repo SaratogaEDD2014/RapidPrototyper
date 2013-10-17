ac43cc6ad3f2b84ec934c96a4152642fd0a9b15a
SkottyK <skrulcik@gmail.com>
2013-10-17 14:03:00 -0400

    Updated to explicit image files

--- GUI/TemplateChooser.py
+++ GUI/TemplateChooser.py
@@ -7,14 +7,13 @@ import BubbleMenu
 import AppSettings
 import wx
 
-class MainMenu(BubbleMenu.BubbleMenu):
+class TemplateChooser(BubbleMenu.BubbleMenu):
     def __init__(self, parent):
         BubbleMenu.BubbleMenu.__init__(self, parent, wx.Bitmap(AppSettings.IMAGE_PATH+"Main/"+"BubbleTitle.png"), "Shape Menu", size=wx.DefaultSize)
-        self.imagePath=AppSettings.IMAGE_PATH+"Main/"
-        self.buttonList=[BubbleMenu.BubbleButton(   self, wx.Bitmap(self.imagePath+"buttonTemplate1.png"), wx.Bitmap(self.imagePath+"buttonPressed1.png")),
-                            BubbleMenu.BubbleButton(self, wx.Bitmap(self.imagePath+"buttonTemplate2.png"), wx.Bitmap(self.imagePath+"buttonPressed2.png")),
-                            BubbleMenu.BubbleButton(self, wx.Bitmap(self.imagePath+"buttonTemplate3.png"), wx.Bitmap(self.imagePath+"buttonPressed3.png")),
-                            BubbleMenu.BubbleButton(self, wx.Bitmap(self.imagePath+"buttonTemplate4.png"), wx.Bitmap(self.imagePath+"buttonPressed4.png")),
-                            BubbleMenu.BubbleButton(self, wx.Bitmap(self.imagePath+"buttonTemplate5.png"), wx.Bitmap(self.imagePath+"buttonPressed5.png")),
-                            BubbleMenu.BubbleButton(self, wx.Bitmap(self.imagePath+"buttonTemplate6.png"), wx.Bitmap(self.imagePath+"buttonPressed6.png"))]
+        self.imagePath=AppSettings.IMAGE_PATH+"/Templates/T_Chooser/"
+        self.buttonList=[BubbleMenu.BubbleButton(   self, wx.Bitmap(self.imagePath+"Extrusion.png"), wx.Bitmap(self.imagePath+"ExtrusionPress.png")),
+                            BubbleMenu.BubbleButton(self, wx.Bitmap(self.imagePath+"Gear.png"), wx.Bitmap(self.imagePath+"GearPress.png")),
+                            BubbleMenu.BubbleButton(self, wx.Bitmap(self.imagePath+"Mug.png"), wx.Bitmap(self.imagePath+"MugPress.png")),
+                            BubbleMenu.BubbleButton(self, wx.Bitmap(self.imagePath+"Ring.png"), wx.Bitmap(self.imagePath+"RingPress.png")),
+                            BubbleMenu.BubbleButton(self, wx.Bitmap(self.imagePath+"Vase.png"), wx.Bitmap(self.imagePath+"VasePress.png"))]
         self.AddMany(self.buttonList)
