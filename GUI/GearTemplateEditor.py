fd6822deab38b990e91ec171f738394ba275eca4
Scott Krulcik <skrulcik@gmail.com>
2013-10-30 22:43:56 -0400

    More test files for plotting

--- GUI/GearTemplateEditor.py
+++ GUI/GearTemplateEditor.py
@@ -9,6 +9,7 @@
 import wx
 import wx.lib.plot as plot
 import AppSettings
+import numpy
 
 class TemplateEditor(wx.Panel):
     def __init__(self, parent, id=-1, position=wx.DefaultPosition, size=wx.Size(800,400)):
@@ -37,19 +38,119 @@ class DrawingView(plot.PlotCanvas):
     def __init__(self, parent, lines=[plot.PolyLine([(1,2), (2,3), (3,5), (4,6), (5,8), (6,8), (10,10)]),plot.PolyLine([(-4,2), (-2,3), (-1,5), (1,8), (2,8), (4,10)])]):
         super(DrawingView, self).__init__(parent)
         self.lines=lines
-        self.draw(self.lines)
+    #self.Bind(wx.EVT_BUTTON, self.draw, id=2)
 
-    def draw(self, lines=None):
-        if lines== None: lines=self.lines
-        gc = plot.PlotGraphics(lines, 'Gear')
-        self.Draw(gc,  xAxis= (0,15), yAxis= (0,15))
+    """def defaultDraw(self, event):
+        gc = plot.PlotGraphics(self.lines, 'Gear')
+        self.Draw(gc,  xAxis= (0,15), yAxis= (0,15))"""
 
-def main():
-    ProtoApp = wx.App()
-    frm = wx.Frame(None, -1, 'line', size=(600,450))
-    DrawingView(frm)
-    frm.Show(True)
-    ProtoApp.MainLoop()
 
+
+
+
+
+
+#-----------------------------------------------------------------------
 if __name__ == '__main__':
-    main()
+    def _InitObjects():
+        # 100 points sin function, plotted as green circles
+        data1 = 2.*numpy.pi*numpy.arange(200)/200.
+        data1.shape = (100, 2)
+        data1[:,1] = numpy.sin(data1[:,0])
+        markers1 = plot.PolyMarker(data1, colour='green', marker='circle',size=1)
+        
+        # 50 points cos function, plotted as red line
+        data1 = 2.*numpy.pi*numpy.arange(100)/100.
+        data1.shape = (50,2)
+        data1[:,1] = numpy.cos(data1[:,0])
+        lines = plot.PolyLine(data1, colour='red')
+        
+        # A few more points...
+        pi = numpy.pi
+        markers2 = plot.PolyMarker([(0., 0.), (pi/4., 1.), (pi/2, 0.),
+                               (3.*pi/4., -1)], colour='blue',
+                              fillcolour='green', marker='cross')
+                              
+        return plot.PlotGraphics([markers1, lines, markers2])
+    
+    
+    class AppFrame(wx.Frame):
+        def __init__(self, parent, id, title):
+            wx.Frame.__init__(self, parent, id, title,
+            wx.DefaultPosition, (400, 400))
+
+            # Now Create the menu bar and items
+            self.mainmenu = wx.MenuBar()
+
+            menu = wx.Menu()
+            menu.Append(200, '&Print...', 'Print the current plot')
+            self.Bind(wx.EVT_MENU, self.OnFilePrint, id=200)
+            menu.Append(209, 'E&xit', 'Enough of this already!')
+            self.Bind(wx.EVT_MENU, self.OnFileExit, id=209)
+            self.mainmenu.Append(menu, '&File')
+
+            menu = wx.Menu()
+            menu.Append(210, '&Draw', 'Draw plots')
+            self.Bind(wx.EVT_MENU,self.OnPlotDraw, id=210)
+            menu.Append(211, '&Redraw', 'Redraw plots')
+            self.Bind(wx.EVT_MENU,self.OnPlotRedraw, id=211)
+            menu.Append(212, '&Clear', 'Clear canvas')
+            self.Bind(wx.EVT_MENU,self.OnPlotClear, id=212)
+            self.mainmenu.Append(menu, '&Plot')
+
+            menu = wx.Menu()
+            menu.Append(220, '&About', 'About this thing...')
+            self.Bind(wx.EVT_MENU, self.OnHelpAbout, id=220)
+            self.mainmenu.Append(menu, '&Help')
+
+            self.SetMenuBar(self.mainmenu)
+
+            # A status bar to tell people what's happening
+            self.CreateStatusBar(1)
+            self.testPanel=wx.Panel(self, pos=(400,400))
+            self.testPanel.SetBackgroundColour(wx.Colour(200,0,0))
+            self.client = DrawingView(self)
+        
+        def OnFilePrint(self, event):
+            d = wx.MessageDialog(self,
+                                 """As of this writing, printing support in wxPython is shaky at best.
+                                     Are you sure you want to do this?""", "Danger!", wx.YES_NO)
+            if d.ShowModal() == wx.ID_YES:
+                psdc = wx.PostScriptDC("out.ps", True, self)
+                self.client.redraw(psdc)
+        
+        def OnFileExit(self, event):
+            self.Close()
+        
+        def OnPlotDraw(self, event):
+            self.client.Draw(_InitObjects(),'automatic','automatic');
+        
+        def OnPlotRedraw(self,event):
+            self.client.redraw()
+        
+        def OnPlotClear(self,event):
+            self.client.last_draw = None
+            dc = wx.ClientDC(self.client)
+            dc.Clear()
+        
+        def OnHelpAbout(self, event):
+            about = wx.MessageDialog(self, __doc__, "About...", wx.OK)
+            about.ShowModal()
+    
+    
+    
+    class MyApp(wx.App):
+        def OnInit(self):
+            frame = AppFrame(None, -1, "wxPlotCanvas")
+            frame.Show(True)
+            self.SetTopWindow(frame)
+            return True
+    
+    
+    app = MyApp(0)
+    app.MainLoop()
+
+
+
+
+#----------------------------------------------------------------------------
--- GUI/TesterII.py
+++ GUI/TesterII.py
@@ -37,22 +37,22 @@ warningmsg = r"""\
 
 warnings.warn(warningmsg, DeprecationWarning, stacklevel=2)
 
-# Not everybody will have Numeric, so let's be cool about it...
+# Not everybody will have numpy, so let's be cool about it...
 try:
-    import Numeric
+    import numpy
 except:
     # bummer!
-    msg = """This module requires the Numeric module, which could not be
-        imported.  It probably is not installed (it's not part of the standard
-        Python distribution). See the Python site (http://www.python.org) for
-        information on downloading source or binaries."""
-    
+    msg = """This module requires the numpy module, which could not be
+    imported.  It probably is not installed (it's not part of the standard
+    Python distribution). See the Python site (http://www.python.org) for
+    information on downloading source or binaries."""
+
     print msg
     if wx.Platform == '__WXMSW__' and wx.GetApp() is not None:
-        d = wx.MessageDialog(None, msg, "Numeric not found")
-        if d.ShowModal() == wx.ID_CANCEL:
-            d = wx.MessageDialog(None, "I kid you not! Pressing Cancel won't help you!", "Not a joke", wx.OK)
-            d.ShowModal()
+        d = wx.MessageDialog(None, msg, "numpy not found")
+    if d.ShowModal() == wx.ID_CANCEL:
+        d = wx.MessageDialog(None, "I kid you not! Pressing Cancel won't help you!", "Not a joke", wx.OK)
+        d.ShowModal()
     raise
 
 #
@@ -61,7 +61,7 @@ except:
 class PolyPoints:
     
     def __init__(self, points, attr):
-        self.points = Numeric.array(points)
+        self.points = numpy.array(points)
         self.scaled = self.points
         self.attributes = {}
         for name, value in self._attributes.items():
@@ -71,8 +71,8 @@ class PolyPoints:
             self.attributes[name] = value
     
     def boundingBox(self):
-        return Numeric.minimum.reduce(self.points), \
-            Numeric.maximum.reduce(self.points)
+        return numpy.minimum.reduce(self.points), \
+            numpy.maximum.reduce(self.points)
     
     def scaleAndShift(self, scale=1, shift=0):
         self.scaled = scale*self.points+shift
@@ -165,8 +165,8 @@ class PlotGraphics:
         p1, p2 = self.objects[0].boundingBox()
         for o in self.objects[1:]:
             p1o, p2o = o.boundingBox()
-            p1 = Numeric.minimum(p1, p1o)
-            p2 = Numeric.maximum(p2, p2o)
+            p1 = numpy.minimum(p1, p1o)
+            p2 = numpy.maximum(p2, p2o)
         return p1, p2
     
     def scaleAndShift(self, scale=1, shift=0):
@@ -225,10 +225,10 @@ class PlotCanvas(wx.Window):
     
     def _setsize(self):
         (self.width,self.height) = self.GetClientSize();
-        self.plotbox_size = 0.97*Numeric.array([self.width, -self.height])
+        self.plotbox_size = 0.97*numpy.array([self.width, -self.height])
         xo = 0.5*(self.width-self.plotbox_size[0])
         yo = self.height-0.5*(self.height+self.plotbox_size[1])
-        self.plotbox_origin = Numeric.array([xo, yo])
+        self.plotbox_origin = numpy.array([xo, yo])
     
     def draw(self, graphics, xaxis = None, yaxis = None, dc = None):
         if dc == None: dc = wx.ClientDC(self)
@@ -263,14 +263,14 @@ class PlotCanvas(wx.Window):
             text_height[1] = max(text_height[1], h)
         else:
             yticks = None
-        text1 = Numeric.array([text_width[0], -text_height[1]])
-        text2 = Numeric.array([text_width[1], -text_height[0]])
+        text1 = numpy.array([text_width[0], -text_height[1]])
+        text2 = numpy.array([text_width[1], -text_height[0]])
         scale = (self.plotbox_size-text1-text2) / (p2-p1)
         shift = -p1*scale + self.plotbox_origin + text1
         self._drawAxes(dc, xaxis, yaxis, p1, p2,
                        scale, shift, xticks, yticks)
-                       graphics.scaleAndShift(scale, shift)
-                       graphics.draw(dc)
+        graphics.scaleAndShift(scale, shift)
+        graphics.draw(dc)
         dc.EndDrawing()
     
     def _axisInterval(self, spec, lower, upper):
@@ -285,8 +285,8 @@ class PlotCanvas(wx.Window):
             range = upper-lower
             if range == 0.:
                 return lower-0.5, upper+0.5
-            log = Numeric.log10(range)
-            power = Numeric.floor(log)
+            log = numpy.log10(range)
+            power = numpy.floor(log)
             fraction = log-power
             if fraction <= 0.05:
                 power = power-1
@@ -311,11 +311,11 @@ class PlotCanvas(wx.Window):
             lower, upper = xaxis
             text = 1
             for y, d in [(bb1[1], -3), (bb2[1], 3)]:
-                p1 = scale*Numeric.array([lower, y])+shift
-                p2 = scale*Numeric.array([upper, y])+shift
+                p1 = scale*numpy.array([lower, y])+shift
+                p2 = scale*numpy.array([upper, y])+shift
                 dc.DrawLine(p1[0],p1[1], p2[0],p2[1])
                 for x, label in xticks:
-                    p = scale*Numeric.array([x, y])+shift
+                    p = scale*numpy.array([x, y])+shift
                     dc.DrawLine(p[0],p[1], p[0],p[1]+d)
                     if text:
                         dc.DrawText(label, p[0],p[1])
@@ -326,11 +326,11 @@ class PlotCanvas(wx.Window):
             text = 1
             h = dc.GetCharHeight()
             for x, d in [(bb1[0], -3), (bb2[0], 3)]:
-                p1 = scale*Numeric.array([x, lower])+shift
-                p2 = scale*Numeric.array([x, upper])+shift
+                p1 = scale*numpy.array([x, lower])+shift
+                p2 = scale*numpy.array([x, upper])+shift
                 dc.DrawLine(p1[0],p1[1], p2[0],p2[1])
                 for y, label in yticks:
-                    p = scale*Numeric.array([x, y])+shift
+                    p = scale*numpy.array([x, y])+shift
                     dc.DrawLine(p[0],p[1], p[0]-d,p[1])
                     if text:
                         dc.DrawText(label,
@@ -339,13 +339,13 @@ class PlotCanvas(wx.Window):
     
     def _ticks(self, lower, upper):
         ideal = (upper-lower)/7.
-        log = Numeric.log10(ideal)
-        power = Numeric.floor(log)
+        log = numpy.log10(ideal)
+        power = numpy.floor(log)
         fraction = log-power
         factor = 1.
         error = fraction
         for f, lf in self._multiples:
-            e = Numeric.fabs(fraction-lf)
+            e = numpy.fabs(fraction-lf)
             if e < error:
                 error = e
                 factor = f
@@ -359,13 +359,13 @@ class PlotCanvas(wx.Window):
             digits = -int(power)
             format = '%'+`digits+2`+'.'+`digits`+'f'
         ticks = []
-        t = -grid*Numeric.floor(-lower/grid)
+        t = -grid*numpy.floor(-lower/grid)
         while t <= upper:
             ticks.append( (t, format % (t,)) )
             t = t + grid
         return ticks
     
-    _multiples = [(2., Numeric.log10(2.)), (5., Numeric.log10(5.))]
+    _multiples = [(2., numpy.log10(2.)), (5., numpy.log10(5.))]
     
     def redraw(self,dc=None):
         if self.last_draw is not None:
@@ -384,19 +384,19 @@ class PlotCanvas(wx.Window):
 if __name__ == '__main__':
     def _InitObjects():
         # 100 points sin function, plotted as green circles
-        data1 = 2.*Numeric.pi*Numeric.arange(200)/200.
+        data1 = 2.*numpy.pi*numpy.arange(200)/200.
         data1.shape = (100, 2)
-        data1[:,1] = Numeric.sin(data1[:,0])
+        data1[:,1] = numpy.sin(data1[:,0])
         markers1 = PolyMarker(data1, color='green', marker='circle',size=1)
         
         # 50 points cos function, plotted as red line
-        data1 = 2.*Numeric.pi*Numeric.arange(100)/100.
+        data1 = 2.*numpy.pi*numpy.arange(100)/100.
         data1.shape = (50,2)
-        data1[:,1] = Numeric.cos(data1[:,0])
+        data1[:,1] = numpy.cos(data1[:,0])
         lines = PolyLine(data1, color='red')
         
         # A few more points...
-        pi = Numeric.pi
+        pi = numpy.pi
         markers2 = PolyMarker([(0., 0.), (pi/4., 1.), (pi/2, 0.),
                                (3.*pi/4., -1)], color='blue',
                               fillcolor='green', marker='cross')
@@ -407,37 +407,38 @@ if __name__ == '__main__':
     class AppFrame(wx.Frame):
         def __init__(self, parent, id, title):
             wx.Frame.__init__(self, parent, id, title,
-                              wx.DefaultPosition, (400, 400))
-                              
-                              # Now Create the menu bar and items
-                              self.mainmenu = wx.MenuBar()
-                              
-                              menu = wx.Menu()
-                              menu.Append(200, '&Print...', 'Print the current plot')
-                              self.Bind(wx.EVT_MENU, self.OnFilePrint, id=200)
-                              menu.Append(209, 'E&xit', 'Enough of this already!')
-                              self.Bind(wx.EVT_MENU, self.OnFileExit, id=209)
-                              self.mainmenu.Append(menu, '&File')
-                              
-                              menu = wx.Menu()
-                              menu.Append(210, '&Draw', 'Draw plots')
-                              self.Bind(wx.EVT_MENU,self.OnPlotDraw, id=210)
-                              menu.Append(211, '&Redraw', 'Redraw plots')
-                              self.Bind(wx.EVT_MENU,self.OnPlotRedraw, id=211)
-                              menu.Append(212, '&Clear', 'Clear canvas')
-                              self.Bind(wx.EVT_MENU,self.OnPlotClear, id=212)
-                              self.mainmenu.Append(menu, '&Plot')
-                              
-                              menu = wx.Menu()
-                              menu.Append(220, '&About', 'About this thing...')
-                              self.Bind(wx.EVT_MENU, self.OnHelpAbout, id=220)
-                              self.mainmenu.Append(menu, '&Help')
-                              
-                              self.SetMenuBar(self.mainmenu)
-                              
-                              # A status bar to tell people what's happening
-                              self.CreateStatusBar(1)
-                              
+            wx.DefaultPosition, (400, 400))
+
+            # Now Create the menu bar and items
+            self.mainmenu = wx.MenuBar()
+
+            menu = wx.Menu()
+            menu.Append(200, '&Print...', 'Print the current plot')
+            self.Bind(wx.EVT_MENU, self.OnFilePrint, id=200)
+            menu.Append(209, 'E&xit', 'Enough of this already!')
+            self.Bind(wx.EVT_MENU, self.OnFileExit, id=209)
+            self.mainmenu.Append(menu, '&File')
+
+            menu = wx.Menu()
+            menu.Append(210, '&Draw', 'Draw plots')
+            self.Bind(wx.EVT_MENU,self.OnPlotDraw, id=210)
+            menu.Append(211, '&Redraw', 'Redraw plots')
+            self.Bind(wx.EVT_MENU,self.OnPlotRedraw, id=211)
+            menu.Append(212, '&Clear', 'Clear canvas')
+            self.Bind(wx.EVT_MENU,self.OnPlotClear, id=212)
+            self.mainmenu.Append(menu, '&Plot')
+
+            menu = wx.Menu()
+            menu.Append(220, '&About', 'About this thing...')
+            self.Bind(wx.EVT_MENU, self.OnHelpAbout, id=220)
+            self.mainmenu.Append(menu, '&Help')
+
+            self.SetMenuBar(self.mainmenu)
+
+            # A status bar to tell people what's happening
+            self.CreateStatusBar(1)
+            self.testPanel=wx.Panel(self, pos=(400,400))
+            self.testPanel.SetBackgroundColour(wx.Colour(200,0,0))
             self.client = PlotCanvas(self)
         
         def OnFilePrint(self, event):
