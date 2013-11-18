29a49921c34fe8d10adc845a5b66a9e64b3e3c30
SkottyK <skrulcik@gmail.com>
2013-11-17 20:56:44 -0500

    Revert "Changed header to proper convention"
    This reverts commit 69470e71518d0c08ad002b3a20238b6336be1b91.

--- GUI/AppSettings.py
+++ GUI/AppSettings.py
@@ -30,7 +30,7 @@ recentFiles=[PATH+"examples/teapot.stl",
 LAYER_DEPTH=.01 #IN INCHES
 
 
-def add_recent_file(filename):
+def addRecentFile(filename):
     global recentFiles
     recentFiles.insert(0,filename)
     if len(recentFiles)>5:
