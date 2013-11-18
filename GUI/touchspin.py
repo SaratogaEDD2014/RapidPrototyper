0076a3959ad7b18b1affbc8e9af8a7b7d6ee4e5f
SkottyK <skrulcik@gmail.com>
2013-11-17 22:27:59 -0500

    touchspin test

--- GUI/touchspin.py
+++ GUI/touchspin.py
@@ -2,39 +2,74 @@ import wx
 import wx.lib.agw.floatspin as FS
 
 class TouchSpin(wx.Control):
-    def __init__(self, parent, id=-1, value=0.0, limits=(0,10), increment=1, pos=wx.DefaultPosition, size=(105,30)):  #TODO: add style/formatting flags for constructor
+    def __init__(self, parent, id=-1, value=0.0, limits=(0,10), increment=1, pos=wx.DefaultPosition, size=wx.DefaultSize):  #TODO: add style/formatting flags for constructor
         super(TouchSpin, self).__init__(parent, id, pos, size)
 
         self._value=value
+        self._range=limits
+        self.increment=increment
         self._textcontrol=wx.TextCtrl(self, -1, "1", (30, 50), (60, -1))
 
-        h = self.text.GetSize().height
-        w = self.text.GetSize().width + self.text.GetPosition().x + 2
+        h = self._textcontrol.GetSize().height
+        w = self._textcontrol.GetSize().width + self._textcontrol.GetPosition().x + 2
 
-        self.spin = wx.SpinButton(self, -1,
-                                  (w, 50),
-                                  (h*2/3, h),
-                                  wx.SP_VERTICAL)
+        self._up = wx.Panel(self, size=(30,30))
+        self._up.SetBackgroundColour(wx.Colour(0,0,255))
+        self._down=wx.Panel(self, size=(30,30))
+        self._down.SetBackgroundColour(wx.Colour(255,0,0))
+
+        sizer=wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self._textcontrol)
+        sizer.Add(self._up)
+        sizer.Add(self._down)
+        self.SetSizer(sizer)
 
         self.Bind(wx.EVT_LEFT_DOWN, self._on_click)
 
     def _on_click(self, event):
-        self.text.SetValue(str(event.GetPosition()))
-
-    def setLimits(self, val):
-        self._range=val
-    def getLimits(self):
-        return self._range
-    self.limits=property(getRange, setRange)
+        x, y = event.GetPosition()
+        if self._up.region.Contains(x, y):
+            self.value+=self._inc
+        elif self._down.region.Contains(x,y):
+            self.value-=self._inc
 
     def setValue(self, val):
         self._value=val
+        if self._value>self.range[1]:
+            self._value=self.range[1]
+        elif self._value<self.range[0]:
+            self._value=self.range[0]
+        self._textcontrol.SetValue(self._value)
     def getValue(self):
         return self._value
-    self.value=property(getValue, setValue)
+    value=property(getValue, setValue)
 
     def setIncrement(self, val):
         self._inc=val
     def getIncrement(self):
         return self._inc
-    self.increment=property(getIncrement, setIncrement)
+    increment=property(getIncrement, setIncrement)
+
+    def setRange(self, val):
+        self._range=val
+    def getRange(self):
+        return self._range
+    range=property(getRange, setRange)
+
+
+def main():
+    ProtoApp = wx.App()
+    frm = wx.Frame(None, -1, 'Gear Display', size=(800,400))
+
+    sizer=wx.BoxSizer(wx.HORIZONTAL)
+    panel=TouchSpin(frm)
+    sizer.Add(panel)
+    panel.Show(True)
+
+    frm.SetSizer(sizer)
+    frm.Show(True)
+    ProtoApp.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
