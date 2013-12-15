738f838988fbd785b08ed8094cd4ff71c611c758
SkottyK <skrulcik@gmail.com>
2013-12-14 22:41:34 -0500

    Attempts to show display

--- GUI/QuickPrint.py
+++ GUI/QuickPrint.py
@@ -1,6 +1,8 @@
 import wx
-import AppSettings
-#from PartViewer import generate_view
+from App import *
+import settings as AppSettings
+from visual import *
+from PartViewer import generate_view
 
 class QuickPrint(wx.Panel):
     def __init__(self, parent, id=-1, pos=(0,40), size=wx.Size(800,440)):
@@ -8,6 +10,32 @@ class QuickPrint(wx.Panel):
         self.SetBackgroundColour(AppSettings.secondBackground)
         self.Show(False)
     def Show(self, visible):
-        super(QuickPrint, self).Show(visible)
-        #if visible:
-        #    AppSettings.set_view(generate_view(self))
+        #super(QuickPrint, self).Show(visible)
+        if visible:
+            AppSettings.main_window.panel.Show(True)
+            disp = display(window=AppSettings.main_window, x=150, y=15, width=400, height=400, forward=-vector(0,1,2))
+            cube = box(color=color.red)
+            #disp.window.win.Set
+            while AppSettings.display_part:
+                rate(100)
+        else:
+            super(QuickPrint, self).Show(visible)
+
+
+def disp_part():
+    disp = display(window=AppSettings.main_window, x=15, y=15, width=400, height=400, forward=-vector(0,1,2))
+    cube = box(color=color.red)
+    while AppSettings.display_part:
+        rate(100)
+    disp._destroy()
+
+def main():
+    ProtoApp = wx.App()
+    frame = ProtoFrame(None, -1, 'Blue Streaks EDD')
+    disp_part()
+    frame.win.Show(True)
+    frame.panel.Show(False)
+    ProtoApp.MainLoop()
+
+if __name__ == '__main__':
+    main()
