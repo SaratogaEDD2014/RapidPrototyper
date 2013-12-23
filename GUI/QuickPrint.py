bcca786937c6a294a4bdfa7d1c1af28a4ee2efdf
Scott Krulcik <skrulcik@gmail.com>
2013-12-22 20:40:17 -0500

    Attempt to destroy window

--- GUI/QuickPrint.py
+++ GUI/QuickPrint.py
@@ -8,18 +8,21 @@ class QuickPrint(wx.Panel):
     def __init__(self, parent, id=-1, pos=(0,40), size=wx.Size(800,440)):
         wx.Panel.__init__(self, parent, id, pos, size)
         self.SetBackgroundColour(AppSettings.secondBackground)
+        self.disp=None
         self.Show(False)
     def Show(self, visible):
         #super(QuickPrint, self).Show(visible)
         if visible:
             AppSettings.main_window.panel.Show(True)
-            disp = display(window=AppSettings.main_window, x=150, y=15, width=400, height=400, forward=-vector(0,1,2))
+            self.disp = display(window=AppSettings.main_window, x=0, y=0, width=400, height=400, forward=-vector(0,1,2))
             cube = box(color=color.red)
             #disp.window.win.Set
             while AppSettings.display_part:
                 rate(100)
         else:
             super(QuickPrint, self).Show(visible)
+            if self.disp != None:
+                self.disp._destroy()
 
 
 def disp_part():
