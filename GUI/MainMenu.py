e2c78e629edeaa49295513866245efde74925235
SkottyK <skrulcik@gmail.com>
2013-12-27 10:10:36 -0500

    Changed non-icon view

--- GUI/MainMenu.py
+++ GUI/MainMenu.py
@@ -21,17 +21,17 @@ class MainMenu(BubbleMenu):
         self.advancedSetupView=AdvancedSetup.AdvancedSetup(self.parent)
         self.templatesView=TemplateChooser.TemplateChooser(self.parent)
         self.openRecentView=OpenRecent.OpenRecent(self.parent)
-        
+
         if(AppSettings.icon_view):
             self.quickPrint=MenuButton(self, wx.Bitmap(self.imagePath+"QuickPrint.png"), wx.Bitmap(self.imagePath+"QuickPrintPress.png"), target=self.quickPrintView)
             self.advancedSetup=MenuButton(self, wx.Bitmap(self.imagePath+"AdvancedSetup.png"), wx.Bitmap(self.imagePath+"AdvancedSetupPress.png"), target= self.advancedSetupView)
             self.templates=MenuButton(self, wx.Bitmap(self.imagePath+"Templates.png"), wx.Bitmap(self.imagePath+"TemplatesPress.png"), target=self.templatesView)
             self.openRecent=MenuButton(self, wx.Bitmap(self.imagePath+"OpenRecent.png"), wx.Bitmap(self.imagePath+"OpenRecentPress.png"), target=self.openRecentView)
         else:
-            self.quickPrint=MenuButton(self, name='Quick\nPrint', target=self.quickPrintView)
-            self.advancedSetup=MenuButton(self,name='Advanced\nSetup', target= self.advancedSetupView)
-            self.templates=MenuButton(self, name='Templates', target=self.templatesView)
-            self.openRecent=MenuButton(self, name='Open\nRecent', target=self.openRecentView)
+            self.quickPrint = DynamicButton(self, name='Quick\nPrint', target=self.quickPrintView)
+            self.advancedSetup = DynamicButton(self,name='Advanced\nSetup', target= self.advancedSetupView)
+            self.templates = DynamicButton(self, name='Templates', target=self.templatesView)
+            self.openRecent = DynamicButton(self, name='Open\nRecent', target=self.openRecentView)
 
         self.buttonList=[self.quickPrint, self.advancedSetup, self.templates, self.openRecent]
         self.setChildren(self.buttonList)
