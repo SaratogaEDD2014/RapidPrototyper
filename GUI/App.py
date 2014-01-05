5dc4a6b60674ca16a541ea8cce55a797665606fe
SkottyK <skrulcik@gmail.com>
2014-01-05 15:05:42 -0500

    Added splash screen
    Close #90
    Added splash screen to run before application and display name of the
    printer.
    It is really just a dummy screen, and not necessary for loading as most
    imports have already completed because the splash screen requires wx.
    
    """Note: most additions to App.py in this commit are involved in testing
    full screen mode.  I could not commit individual lines, so I included
    them in this commit just to get this in on time.

--- GUI/App.py
+++ GUI/App.py
@@ -3,17 +3,20 @@ import wx
 import GUI.settings as settings
 import ProtoToolbar
 import MainMenu
+import GUI.splash_screen
 from visual import *
 
 
 class ProtoFrame(window):
     def __init__(self, parent, id, title):
         #super(ProtoFrame, self).__init__(parent, id, title, size=wx.Size(800, 480))
-        window.__init__(self, width=800, height=480, x=-8, y=-30, title=title)
+        window.__init__(self, width=800, height=480, x=8, y=30, title=title)
+        settings.icon_view = False
+        self.win.Show(False)
         self.win.SetBackgroundColour(settings.defaultBackground)
+        self.win.Bind(wx.EVT_SIZE, self.OnSize)
         self.imagePath=settings.IMAGE_PATH+"Main/"
         self.title=title
-        settings.icon_view = False
         self.toolbar=ProtoToolbar.ProtoToolbar(self.win)
         self.toolbar.Show(True)
         self.menu=MainMenu.MainMenu(self.win)
@@ -21,37 +24,42 @@ class ProtoFrame(window):
         settings.main_window=self
         settings.refresh_view_panel=self.refresh_view_panel
         settings.set_view(self.menu)
-        #disp = display(window=settings.main_window, x=15, y=15, width=400, height=400, forward=-vector(0,1,2))
-        self.panel=wx.Panel(self.win, pos=(0, 80), size=(800,400))
-        self.panel.Show(False)
+        self.win.Maximize()
+        self.win.Show(True)
 
+    def OnSize(self, event):
+        event.Skip()
+        w,h = self.win.GetSize()
+        settings.app_w = w
+        settings.app_h = h-settings.toolbar_h
+        settings.refresh_view_panel()
 
     def set_view(self, viewPanel):
         if(viewPanel!=None):
             settings.add_prev_page(settings.get_current_page())
             settings.set_current_page(viewPanel)
             settings.refresh_view_panel()
-            #self.panel=settings.get_current_page()
+            load=wx.Panel(self.win, pos=(0,0), size=self.win.GetSize())
+            self.win.SendSizeEvent()
+            load.Destroy()
 
     def refresh_view_panel(self):
         mastersizer=wx.BoxSizer(wx.VERTICAL)
+        self.toolbar.Refresh()
         mastersizer.Add(self.toolbar)
-        sizer=wx.BoxSizer()
+        sizer=wx.BoxSizer(wx.HORIZONTAL)
         current=settings.get_current_page()
-        sizer.Add(wx.Panel(self.win, size=((800-current.GetSize()[0])/2,20)))
+        sizer.Add(wx.Panel(self.win, size=((self.win.GetSize()[0]-current.GetSize()[0])/2,20)))
         sizer.Add(current)
         mastersizer.Add(sizer)
-        load=wx.Panel(self.win, pos=(0,40), size=(800,400))
         self.win.SetSizer(mastersizer)
-        self.win.SendSizeEvent()    #Without this, panels will be misaligned
-        load.Destroy()
 
 
 def main():
     ProtoApp = wx.App()
     frame = ProtoFrame(None, -1, 'Blue Streaks EDD')
-    frame.win.Show(True)
     ProtoApp.MainLoop()
 
 if __name__ == '__main__':
+    GUI.splash_screen.show_splash(settings.NAME)
     main()
